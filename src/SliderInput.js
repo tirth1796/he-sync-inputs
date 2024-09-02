f18bb7982d9e5a7daaab972f3c9cc6ec1e701f5c
Tirth Shah <tirth.shah@sprinklr.com>
2024-09-02 19:30:40 +0530

    style fixes

--- src/SliderInput.js
+++ src/SliderInput.js
@@ -1,18 +1,17 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export const SliderInput = ({ min, max, onSubmit }) => {
   const [value, setValue] = useState();
 
-  const onChange = (event) => {
-  };
+  const onChange = (event) => {};
 
   const onSubmitClick = () => {
     onSubmit(value);
   };
 
   return (
-    <div>
-      <div className="flex">
+    <div className="w-full h-full flex flex-col items-center justify-center gap-4">
+      <div className="flex gap-4">
         <input
           role="slider"
           type="range"
@@ -20,17 +19,24 @@ export const SliderInput = ({ min, max, onSubmit }) => {
           min={min}
           max={max}
           onChange={onChange}
+          className=""
         />
         <input
+          placeholder="Enter a Number"
           role="number"
           type="number"
           value={value}
           min={min}
           max={max}
           onChange={onChange}
+          className="border rounded border-solid border-gray-600 w-[140px]"
         />
       </div>
-      <button disabled role="button" onClick={onSubmitClick}>
+      <button
+        role="button"
+        onClick={onSubmitClick}
+        className="bg-blue-600 rounded p-2 text-white"
+      >
         Submit
       </button>
     </div>
